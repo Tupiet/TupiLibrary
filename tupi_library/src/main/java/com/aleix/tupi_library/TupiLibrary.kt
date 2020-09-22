360a992f2e9a9ea0f8cfa3ab656163fcd6df13e4
Tupiet <Tupiet@users.noreply.github.com>
2020-09-22 19:15:26 +0200

    Added a new function

--- tupi_library/src/main/java/com/aleix/tupi_library/TupiLibrary.kt
+++ tupi_library/src/main/java/com/aleix/tupi_library/TupiLibrary.kt
@@ -1,10 +1,17 @@
 package com.aleix.tupi_library
 
+import android.content.Context
+import android.widget.Toast
+
 class TupiLibrary {
 
     fun hello(first: Int, second: Int): Int {
 
         val result = first + second
         return result
+    }
+
+    fun toast(context: Context, text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 }
