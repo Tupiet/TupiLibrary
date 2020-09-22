f7191079a3f0665227809e6e9cd35c1c479d44ba
Tupiet <Tupiet@users.noreply.github.com>
2020-09-22 19:44:08 +0200

    Added the alert function

--- tupi_library/src/main/java/com/aleix/tupi_library/TupiLibrary.kt
+++ tupi_library/src/main/java/com/aleix/tupi_library/TupiLibrary.kt
@@ -2,6 +2,7 @@ package com.aleix.tupi_library
 
 import android.content.Context
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class TupiLibrary {
 
@@ -13,5 +14,16 @@ class TupiLibrary {
 
     fun toast(context: Context, text: String) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
+
+    fun alert(context: Context, title: String, message: String) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(title)
+        builder.setMessage(message)
+
+        builder.setNeutralButton("Ok") { dialog, which ->
+
+        }
+        builder.show()
     }
 }
