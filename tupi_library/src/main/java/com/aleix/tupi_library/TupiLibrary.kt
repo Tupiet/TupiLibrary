e0c9944cd686cf1953ae45161b20986d0bdb7ac4
Tupiet <Tupiet@users.noreply.github.com>
2020-09-26 18:56:41 +0200

    Added the hideKey function

--- tupi_library/src/main/java/com/aleix/tupi_library/TupiLibrary.kt
+++ tupi_library/src/main/java/com/aleix/tupi_library/TupiLibrary.kt
@@ -1,6 +1,8 @@
 package com.aleix.tupi_library
 
 import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
@@ -19,5 +21,14 @@ class TupiLibrary {
 
         }
         builder.show()
+    }
+
+    fun View.hideKeyboard() {
+        val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+    fun hideKey(view: View) {
+        view.hideKeyboard()
     }
 }
